8ef2d1239fecad033eba0622c9bc2cf41022e14a
Benjamin <stealcase@hotmail.com>
2022-11-05 19:23:19 +0100

    Update .gitingore

---
+++ src/views/OAuth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="oauth">
+    <h1>Thanks for Signing in!</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .oauth {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
